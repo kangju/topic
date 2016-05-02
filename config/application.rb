611ab50417d377c6ca913fc609550af037c858fa
kangju <kangju0815@gmail.com>
2016-05-02 13:22:24 +0900

    rspec指定削除

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,6 @@ module Topic
     config.generators do |g|
         g.helper false
         g.assets false
-        g.test_framework :rspec
         g.controller_specs false
         g.view_specs false
     end
